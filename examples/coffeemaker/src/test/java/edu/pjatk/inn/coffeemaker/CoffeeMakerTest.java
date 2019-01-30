3d984fca0a920b4ab3bbc96b75e93a38e7e17cc1
Jakub Fryga <JakubFryga@simple.com.pl>
2019-01-30 22:22:48 +0100

    Napisanie testów

--- examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/CoffeeMakerTest.java
+++ examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/CoffeeMakerTest.java
@@ -1,5 +1,6 @@
 package edu.pjatk.inn.coffeemaker;
 
+import com.sun.tools.internal.ws.wsdl.document.jaxws.Exception;
 import edu.pjatk.inn.coffeemaker.impl.CoffeeMaker;
 import edu.pjatk.inn.coffeemaker.impl.Inventory;
 import edu.pjatk.inn.coffeemaker.impl.Recipe;
@@ -14,6 +15,7 @@ import sorcer.service.ContextException;
 import sorcer.service.Exertion;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static sorcer.eo.operator.*;
 
@@ -83,19 +85,19 @@ public class CoffeeMakerTest {
 	}
 
 	@Test
-	public void addRecepie() throws Exception {
+	public void addRecepie() throws java.lang.Exception {
 		coffeeMaker.addRecipe(mocha);
 		assertEquals(coffeeMaker.getRecipeForName("mocha").getName(), "mocha");
 	}
 
 	@Test
-	public void addContextRecepie() throws Exception {
+	public void addContextRecepie() throws java.lang.Exception {
 		coffeeMaker.addRecipe(Recipe.getContext(mocha));
 		assertEquals(coffeeMaker.getRecipeForName("mocha").getName(), "mocha");
 	}
 
 	@Test
-	public void addServiceRecepie() throws Exception {
+	public void addServiceRecepie() throws java.lang.Exception {
 		Exertion cmt = task(sig("addRecipe", coffeeMaker),
 						context(types(Recipe.class), args(espresso),
 							result("recipe/added")));
@@ -105,7 +107,7 @@ public class CoffeeMakerTest {
 	}
 
 	@Test
-	public void addRecipes() throws Exception {
+	public void addRecipes() throws java.lang.Exception {
 		coffeeMaker.addRecipe(mocha);
 		coffeeMaker.addRecipe(macchiato);
 		coffeeMaker.addRecipe(americano);
@@ -116,9 +118,109 @@ public class CoffeeMakerTest {
 	}
 
 	@Test
-	public void makeCoffee() throws Exception {
+	public void makeCoffee() throws java.lang.Exception {
 		coffeeMaker.addRecipe(espresso);
 		assertEquals(coffeeMaker.makeCoffee(espresso, 200), 150);
+	}
+
+	@Test
+	public void testDeleteRecipe() throws java.lang.Exception{
+		assertFalse(coffeeMaker.deleteRecipe(mocha));
+		coffeeMaker.addRecipe(mocha);
+		assertTrue(coffeeMaker.deleteRecipe(mocha));
+		coffeeMaker.addRecipe(mocha);
+	}
+
+	@Test
+	public void testEditRecipe()throws java.lang.Exception {
+		assertFalse(coffeeMaker.editRecipe(espresso,mocha));
+		//coffeeMaker.addRecipe(espresso);
+		//assertTrue(coffeeMaker.editRecipe(espresso,mocha));
+		assertEquals(coffeeMaker.getRecipeForName("mocha"),mocha);
+	}
+
+	@Test
+	public void testAddInventory() throws java.lang.Exception{
+		//An inventory is initialized with 15 units of each ingredient
+		assertTrue(coffeeMaker.addInventory(0,0,0,0));
+		assertEquals(coffeeMaker.checkInventory().getChocolate(), 15);
+		assertEquals(coffeeMaker.checkInventory().getCoffee(), 15);
+		assertEquals(coffeeMaker.checkInventory().getMilk(), 15);
+		assertEquals(coffeeMaker.checkInventory().getSugar(), 15);
+
+		assertFalse(coffeeMaker.addInventory(-1,0,0,0));
+		assertEquals(coffeeMaker.checkInventory().getChocolate(), 15);
+		assertEquals(coffeeMaker.checkInventory().getCoffee(), 15);
+		assertEquals(coffeeMaker.checkInventory().getMilk(), 15);
+		assertEquals(coffeeMaker.checkInventory().getSugar(), 15);
+
+		assertFalse(coffeeMaker.addInventory(0,-1,0,0));
+		assertEquals(coffeeMaker.checkInventory().getChocolate(), 15);
+		assertEquals(coffeeMaker.checkInventory().getCoffee(), 15);
+		assertEquals(coffeeMaker.checkInventory().getMilk(), 15);
+		assertEquals(coffeeMaker.checkInventory().getSugar(), 15);
+
+		assertFalse(coffeeMaker.addInventory(0,0,-1,0));
+		assertEquals(coffeeMaker.checkInventory().getChocolate(), 15);
+		assertEquals(coffeeMaker.checkInventory().getCoffee(), 15);
+		assertEquals(coffeeMaker.checkInventory().getMilk(), 15);
+		assertEquals(coffeeMaker.checkInventory().getSugar(), 15);
+
+		assertFalse(coffeeMaker.addInventory(0,0,0,-1));
+		assertEquals(coffeeMaker.checkInventory().getChocolate(), 15);
+		assertEquals(coffeeMaker.checkInventory().getCoffee(), 15);
+		assertEquals(coffeeMaker.checkInventory().getMilk(), 15);
+		assertEquals(coffeeMaker.checkInventory().getSugar(), 15);
+	}
+
+	@Test
+	public void testCheckInventory() throws java.lang.Exception{
+		assertEquals(coffeeMaker.checkInventory().getChocolate(), 15);
+		assertEquals(coffeeMaker.checkInventory().getCoffee(), 15);
+		assertEquals(coffeeMaker.checkInventory().getMilk(), 15);
+		assertEquals(coffeeMaker.checkInventory().getSugar(), 15);
+	}
+
+	@Test
+	public void testMakeCoffee() throws java.lang.Exception{
+		coffeeMaker.addRecipe(espresso);
+		coffeeMaker.addRecipe(mocha);
+		coffeeMaker.addRecipe(macchiato);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 100), 50);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 45), 45);
+		assertEquals(coffeeMaker.makeCoffee(americano, 60), 60);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 100), 50);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 100), 50);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 100), 50);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 100), 50);
+		assertEquals(coffeeMaker.makeCoffee(espresso, 100), 100);
+	}
+
+	@Test
+	public void testGetRecipes() throws java.lang.Exception{
+		Recipe r[] = coffeeMaker.getRecipes();
+
+		coffeeMaker.addRecipe(espresso);
+		coffeeMaker.addRecipe(mocha);
+		coffeeMaker.addRecipe(macchiato);
+		assertEquals(r[0], macchiato);
+		assertEquals(r[1], mocha);
+		assertEquals(r[2], espresso);
+	}
+
+	@Test
+	public void testGetRecipeForName() throws java.lang.Exception{
+		coffeeMaker.addRecipe(espresso);
+		assertEquals(coffeeMaker.getRecipeForName("Coffee"), espresso);
+		assertEquals(coffeeMaker.getRecipeForName("Mocha"), null);
+	}
+
+	@Test
+	public void testGetRecipeFull() throws java.lang.Exception{
+		boolean [] recipeFull = coffeeMaker.getRecipeFull();
+		for(int i = 0; i < recipeFull.length; i++) {
+			assertFalse(recipeFull[i]);
+		}
 	}
 
 }
