d5339cd0ffccbf002877f3da6d166a935b7dbf65
MatWolf <matwolf95@gmail.com>
2019-01-30 22:17:40 +0100

    Added tests for recipe

--- examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/RecipeTest.java
+++ examples/coffeemaker/src/test/java/edu/pjatk/inn/coffeemaker/RecipeTest.java
@@ -3,6 +3,7 @@ package edu.pjatk.inn.coffeemaker;
 import edu.pjatk.inn.coffeemaker.impl.CoffeeMaker;
 import edu.pjatk.inn.coffeemaker.impl.Inventory;
 import edu.pjatk.inn.coffeemaker.impl.Recipe;
+import junit.framework.TestCase;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -20,25 +21,130 @@ import static sorcer.eo.operator.*;
 
 @RunWith(SorcerTestRunner.class)
 @ProjectContext("examples/coffeemaker")
-public class RecipeTest {
+public class RecipeTest extends TestCase {
     private final static Logger logger = LoggerFactory.getLogger(CoffeeMakerTest.class);
 
-    @Before
-    public void setUp() throws ContextException {
+    private Recipe r1;
+    private Recipe r2;
+
+    protected void setUp() throws Exception {
+        r1 = new Recipe();
+        r2 = new Recipe();
+        super.setUp();
+    }
+    public void testGetAmtChocolate() {
+        assertEquals(0, r1.getAmtChocolate());
     }
 
-
-    @Test
-    public void testAddRecipe() throws Exception {
-
+    public void testSetAmtChocolate() {
+        //Equivalence class - neg
+        r1.setAmtChocolate(-5);
+        assertEquals(0,r1.getAmtChocolate());
+        //Equivalence class - pos
+        r1.setAmtChocolate(5);
+        assertEquals(5,r1.getAmtChocolate());
+        //Boundary
+        r1.setAmtChocolate(-1);
+        assertEquals(5,r1.getAmtChocolate());
+        r1.setAmtChocolate(0);
+        assertEquals(0,r1.getAmtChocolate());
+        r1.setAmtChocolate(1);
+        assertEquals(1,r1.getAmtChocolate());
     }
 
-    @Test
-    public void testEditRecipe() throws Exception {
-
+    public void testGetAmtCoffee() {
+        assertEquals(0, r1.getAmtCoffee());
     }
-    @Test
-    public void testDeleteRecipe() throws  Exception {
 
+    public void testSetAmtCoffee() {
+        r1.setAmtCoffee(-5);
+        assertEquals(0,r1.getAmtCoffee());
+        r1.setAmtCoffee(5);
+        assertEquals(5,r1.getAmtCoffee());
+        r1.setAmtCoffee(-1);
+        assertEquals(5,r1.getAmtCoffee());
+        r1.setAmtCoffee(0);
+        assertEquals(0,r1.getAmtCoffee());
+        r1.setAmtCoffee(1);
+        assertEquals(1,r1.getAmtCoffee());
     }
+
+    public void testGetAmtMilk() {
+        assertEquals(0, r1.getAmtMilk());
+    }
+
+    public void testSetAmtMilk() {
+        r1.setAmtMilk(-5);
+        assertEquals(0,r1.getAmtMilk());
+        r1.setAmtMilk(5);
+        assertEquals(5,r1.getAmtMilk());
+        r1.setAmtMilk(-1);
+        assertEquals(5,r1.getAmtMilk());
+        r1.setAmtMilk(0);
+        assertEquals(0,r1.getAmtMilk());
+        r1.setAmtMilk(1);
+        assertEquals(1,r1.getAmtMilk());
+    }
+
+    public void testGetAmtSugar() {
+        assertEquals(0, r1.getAmtSugar());
+    }
+
+    public void testSetAmtSugar() {
+        r1.setAmtSugar(-5);
+        assertEquals(0,r1.getAmtSugar());
+        r1.setAmtSugar(5);
+        assertEquals(5,r1.getAmtSugar());
+        r1.setAmtSugar(-1);
+        assertEquals(5,r1.getAmtSugar());
+        r1.setAmtSugar(0);
+        assertEquals(0,r1.getAmtSugar());
+        r1.setAmtSugar(1);
+        assertEquals(1,r1.getAmtSugar());
+    }
+
+    public void testGetName() {
+        assertEquals("", r1.getName());
+    }
+
+    public void testSetName() {
+        r1.setName("Coffee");
+        assertEquals("Coffee", r1.getName());
+    }
+
+    public void testGetPrice() {
+        assertEquals(0, r1.getPrice());
+    }
+
+    public void testSetPrice() {
+        r1.setPrice(50);
+        assertEquals(50, r1.getPrice());
+    }
+
+    /*
+     * Class under test for boolean equals(Recipe)
+     */
+    public void testEqualsRecipe() {
+        r1.setName(null);
+        r2.setName("asdf");
+        assertFalse(r1.equals(r2));
+        assertFalse(r2.equals(r1));
+        r1.setName("jkl");
+        assertFalse(r1.equals(r2));
+        assertFalse(r2.equals(r1));
+        r1.setName("asdf");
+        assertTrue(r1.equals(r2));
+        assertTrue(r2.equals(r1));
+    }
+
+    /*
+     * Class under test for String toString()
+     */
+    public void testToString() {
+        r1.setName(null);
+        assertEquals(r1.toString(), "");
+        r1.setName("asdf");
+        assertEquals(r1.toString(), "asdf");
+    }
+
 }
