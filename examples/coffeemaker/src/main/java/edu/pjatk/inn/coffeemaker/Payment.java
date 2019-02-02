88bd508b16a827842318c92421dc7d0f101ddca5
MatWolf <matwolf95@gmail.com>
2019-02-02 21:04:50 +0100

    Added  payment module

---
+++ examples/coffeemaker/src/main/java/edu/pjatk/inn/coffeemaker/Payment.java
@@ -0,0 +1,10 @@
+package edu.pjatk.inn.coffeemaker;
+
+import sorcer.service.Context;
+import sorcer.service.ContextException;
+
+import java.rmi.RemoteException;
+
+public interface Payment {
+    public Context pay(Context context) throws  RemoteException, ContextException;
+}
